30969df0efa3cf4e6bf16934b4f62c32ad2824d9
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-10-20 11:24:06 -0600

    remove set -e

--- edge/roles/edge_csr_approver/files/csr_approver.sh
+++ edge/roles/edge_csr_approver/files/csr_approver.sh
@@ -2,7 +2,6 @@
 
 # see https://docs.openshift.com/container-platform/4.11/backup_and_restore/control_plane_backup_and_restore/disaster_recovery/scenario-3-expired-certs.html
 
-set -e
 set -o pipefail
 
 export PATH=${PATH}:/usr/bin # make sure path includes /usr/bin
