3cb96318d032231e16e8f951a9fe749176ebac8d
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-03-06 15:56:00 -0700

    better script

--- edge/post_scripts_mirror_example/01_catalogsource.sh
+++ edge/post_scripts_mirror_example/01_catalogsource.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
+oc_version=$(oc get clusterversion version -o jsonpath='{.status.desired.version}' | awk -F. '{print $1"."$2}')
+
 oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
 
 oc wait --for=delete catalogsource/community-operators -n openshift-marketplace
@@ -11,6 +15,6 @@ metadata:
   name: redhat-operators
   namespace: openshift-marketplace
 spec:
-  image: <mirror_hostname>:8443/redhat/redhat-operator-index:v4.12
+  image: <mirror_hostname>:8443/redhat/redhat-operator-index:v${oc_version}
   sourceType: grpc
 EOF
