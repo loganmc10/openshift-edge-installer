bd7364180927385563b310727d5fcb0c1f815877
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-03-06 15:00:04 -0700

    update example

--- edge/post_scripts_mirror_example/catalogsource.sh
+++ edge/post_scripts_mirror_example/catalogsource.sh
@@ -2,7 +2,7 @@
 
 oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
 
-oc wait --for=delete catalogsource/redhat-operators -n openshift-marketplace --timeout=15s || true
+oc wait --for=delete catalogsource/community-operators -n openshift-marketplace
 
 cat << EOF | oc apply -f -
 apiVersion: operators.coreos.com/v1alpha1
