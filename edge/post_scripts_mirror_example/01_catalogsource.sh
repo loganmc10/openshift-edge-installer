f95667d82fe6fde670d814871b3716a4584f1059
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-03-06 16:37:49 -0700

    use variable in example

--- edge/post_scripts_mirror_example/01_catalogsource.sh
+++ edge/post_scripts_mirror_example/01_catalogsource.sh
@@ -2,6 +2,7 @@
 
 set -o pipefail
 
+mirror_hostname="<mirror_hostname>"
 oc_version=$(oc get clusterversion version -o jsonpath='{.status.desired.version}' | awk -F. '{print $1"."$2}')
 
 oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
@@ -15,6 +16,6 @@ metadata:
   name: redhat-operators
   namespace: openshift-marketplace
 spec:
-  image: <mirror_hostname>:8443/redhat/redhat-operator-index:v${oc_version}
+  image: ${mirror_hostname}:8443/redhat/redhat-operator-index:v${oc_version}
   sourceType: grpc
 EOF
