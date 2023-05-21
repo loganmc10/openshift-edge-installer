f30d06cf5fed38e0978c347ebb89d00d257f792e
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-05-21 11:05:58 -0600

    update script

--- edge/roles/edge_csr_approver/files/csr_approver.sh
+++ edge/roles/edge_csr_approver/files/csr_approver.sh
@@ -4,14 +4,16 @@
 
 set -o pipefail
 
+CSR_KUBECONFIG=/var/local/csr_approver/kubeconfig
+NODE_KUBECONFIG=/etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs/localhost.kubeconfig
+
 # wait until API is online
-until oc --request-timeout=30s --kubeconfig /var/local/csr_approver/kubeconfig get csr; do
+until oc --request-timeout=30s --kubeconfig "${CSR_KUBECONFIG}" get csr; do
   sleep 10
 done
 
 go_template='{{range .items}}{{if not .status}}{{if or (eq .spec.signerName "kubernetes.io/kubelet-serving") (eq .spec.signerName "kubernetes.io/kube-apiserver-client-kubelet")}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}'
-until oc --request-timeout=30s --kubeconfig /etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs/localhost.kubeconfig get node; do
-  oc --request-timeout=30s --kubeconfig /var/local/csr_approver/kubeconfig get csr -o go-template="${go_template}" \
-    | xargs --no-run-if-empty oc --request-timeout=30s --kubeconfig /var/local/csr_approver/kubeconfig adm certificate approve
+until oc --request-timeout=30s --kubeconfig "${NODE_KUBECONFIG}" get node; do
+  oc --request-timeout=30s --kubeconfig "${CSR_KUBECONFIG}" get csr -o go-template="${go_template}" | xargs --no-run-if-empty oc --request-timeout=30s --kubeconfig "${CSR_KUBECONFIG}" adm certificate approve
   sleep 20
 done
