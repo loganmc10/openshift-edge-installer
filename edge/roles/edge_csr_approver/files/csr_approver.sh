ec2831056ed78af69a141f108b1139c74be4ad4b
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-05-21 10:37:58 -0600

    fix script

--- edge/roles/edge_csr_approver/files/csr_approver.sh
+++ edge/roles/edge_csr_approver/files/csr_approver.sh
@@ -11,6 +11,7 @@ done
 
 go_template='{{range .items}}{{if not .status}}{{if or (eq .spec.signerName "kubernetes.io/kubelet-serving") (eq .spec.signerName "kubernetes.io/kube-apiserver-client-kubelet")}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}'
 until oc --request-timeout=30s --kubeconfig /etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs/localhost.kubeconfig get node; do
-  oc --request-timeout=30s --kubeconfig /var/local/csr_approver/kubeconfig get csr -o go-template="${go_template}" | xargs --no-run-if-empty oc --request-timeout=30s adm certificate approve
+  oc --request-timeout=30s --kubeconfig /var/local/csr_approver/kubeconfig get csr -o go-template="${go_template}" \
+    | xargs --no-run-if-empty oc --request-timeout=30s --kubeconfig /var/local/csr_approver/kubeconfig adm certificate approve
   sleep 20
 done
