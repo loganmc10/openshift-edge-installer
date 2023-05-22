f03232f70da8dfae0c7b4b657cfee44bf7f7a73d
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-05-21 21:18:07 -0700

    Clean up CSR approver (#49)
    * Clean up CSR approver
    
    * wait for all pods to be ready before clearing registry
    
    * fix script
    
    * add more retries
    
    * update script

--- edge/roles/edge_csr_approver/files/csr_approver.sh
+++ edge/roles/edge_csr_approver/files/csr_approver.sh
@@ -4,19 +4,16 @@
 
 set -o pipefail
 
-export KUBECONFIG="/var/local/csr_approver/kubeconfig"
+CSR_KUBECONFIG=/var/local/csr_approver/kubeconfig
+NODE_KUBECONFIG=/etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs/localhost.kubeconfig
 
 # wait until API is online
-until oc --request-timeout=30s get csr; do
+until oc --request-timeout=30s --kubeconfig "${CSR_KUBECONFIG}" get csr; do
   sleep 10
 done
 
-count=30
 go_template='{{range .items}}{{if not .status}}{{if or (eq .spec.signerName "kubernetes.io/kubelet-serving") (eq .spec.signerName "kubernetes.io/kube-apiserver-client-kubelet")}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}{{end}}'
-while [[ ${count} -gt 0 ]]; do
-  oc --request-timeout=30s get csr -o go-template="${go_template}" | xargs --no-run-if-empty oc --request-timeout=30s adm certificate approve
+until oc --request-timeout=30s --kubeconfig "${NODE_KUBECONFIG}" get node; do
+  oc --request-timeout=30s --kubeconfig "${CSR_KUBECONFIG}" get csr -o go-template="${go_template}" | xargs --no-run-if-empty oc --request-timeout=30s --kubeconfig "${CSR_KUBECONFIG}" adm certificate approve
   sleep 20
-  count=$((count - 1))
-  echo "${count} checks remaining"
 done
-echo "CSR Approver complete"
